72817afe6663a19fd79a66a6229c2671f9b653b4
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2014-05-07 11:56:45 +0100

    radtoncf: Add filtering options
    NB: The command syntax has changed for rad3toncf

--- rad3toncf.f90
+++ rad3toncf.f90
@@ -9,31 +9,66 @@ program rad3toncf
    implicit none
    type(rad3trace) :: tr
    character(len=250) :: infile, outfile
+   real(8) :: lin_gain, exp_gain
+   integer :: start_samp
+   logical :: rmean = .false., gain = .false., outfile_set = .false.
 
    call get_args
 
    call rad3_load(infile, tr)
+
+   if (rmean) call rad3_remove_mean(tr)
+   if (gain) call rad3_gain(tr, start_samp, lin_gain, exp_gain)
+
    call rad3_save_netcdf(tr, outfile)
 
 contains
    subroutine usage()
       write(0,'(a)') &
-         'Usage: rad3toncf [infile] (outfile)', &
+         'Usage: rad3toncf (options) [infile]', &
          'Do not supply the .rd3 extension', &
-         'Default output is to file.nc'
+         'Options:', &
+         '   -gain [start_samp lin_gain exp_gain]' ,&
+         '                 : Apply gain to trace [none]', &
+         '   -o [outfile]  : Send output to outfile [infile.nc]', &
+         '   -rmean        : Remove mean trace [no not remove]', &
+         '   -v            : Verbose output sent to stderr'
       stop
    end subroutine usage
 
    subroutine get_args()
-      if (command_argument_count() == 1) then
-         call get_command_argument(1, infile)
-         outfile = trim(infile) // '.nc'
-      else if (command_argument_count() == 2) then
-         call get_command_argument(1, infile)
-         call get_command_argument(2, outfile)
-      else
-         call usage
-      endif
+      character(len=250) :: arg
+      integer :: iarg, narg
+      narg = command_argument_count()
+      if (narg < 1) call usage
+      call get_command_argument(narg, infile)
+      iarg = 1
+      do while (iarg < narg)
+         call get_command_argument(iarg, arg)
+         select case(arg)
+            case('-gain')
+               call get_command_argument(iarg+1, arg)
+               read(arg,*) start_samp
+               call get_command_argument(iarg+2, arg)
+               read(arg,*) lin_gain
+               call get_command_argument(iarg+3, arg)
+               read(arg,*) exp_gain
+               iarg = iarg + 4
+            case('-o')
+               call get_command_argument(iarg+1, outfile)
+               outfile_set = .true.
+               iarg = iarg + 2
+            case('-rmean')
+               rmean = .true.
+               iarg = iarg + 1
+            case('-v')
+               call rad3_set_verbose
+               iarg = iarg + 1
+            case default
+               call usage
+         end select
+      enddo
+      if (.not.outfile_set) outfile = trim(infile) // '.nc'
    end subroutine get_args
 end program rad3toncf
 !-------------------------------------------------------------------------------
