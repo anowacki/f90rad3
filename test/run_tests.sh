37ca00810599bf662dd26944b01a77ffb2e292b2
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2014-05-12 13:11:30 +0100

    Begin testing framework
    The test/run_tests.sh script runs all the tests in the
    test directory, and exits 0 if all pass, 1 otherwise.

---
+++ test/run_tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Run all the tests and exit with value > 0 if any fail.
+# Invoke with any argument to get verbose output
+
+if [[ "$PWD" != */test ]]; then
+	echo "$(basename $0) must be run in the test directory" >&2
+	exit 1
+fi
+
+B=../bin
+
+mark_fail () {
+	[ -z "$err" ] &&
+		{ echo "Error: \$err must be set before running mark_fail" >&2; exit 1; }
+	[ $err -gt 0 ] && fail=1
+}
+
+pass_fail () {
+	[ $err -eq 0 ] && echo "pass" || echo "FAIL"
+}
+
+run_test () {
+	# Run a program which outputs the string 'failed' if the test fails
+	# Because we're testing Fortran programs, there is no portable way
+	# of setting an exit status at the moment.
+	# Supply one argument, the name of the program: it is assumed to be
+	# in the $B directory
+	echo -n "$1: "
+	"$B/$1" 2>&1 | grep -iqv "failed"; err=$?
+	pass_fail
+	mark_fail
+}
+
+run_test test_save
+
+# Set error state
+[ -z "$fail" ] && exit 0 || exit 1
